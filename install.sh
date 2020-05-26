7427b6a86a8cfb1b09ebe838382e46146b96adb7
Carter C <cclements3150@gmail.com>
2020-05-26 04:14:37 -0400

    [INIT] Run me to install: it just appends a linked file to the bin thing

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Run me!
+
+ln intranet_scraper.py cody
+mv cody /usr/local/bin
+
+echo "All set!"
+echo "Usage: cody <project url or just project number>"
+echo "-_-"
+echo "Visit the README.md for examples, github links and more"
