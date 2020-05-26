82e97013b68dab767f11a845e1e90c4fb9cf8e79
Carter C <cclements3150@gmail.com>
2020-05-26 04:15:01 -0400

    [INIT] Removes the cody file from your bin file DOES NOT DELETE THE DOWNLOAD FOLDER

---
+++ uninstall.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Running me will remove the cody script from /usr/local/bin/ directory.
+# However i will not delete these files, you can do that urself.
+
+rm /usr/local/bin/cody
+
+echo "If cody existed, he's gone now."
